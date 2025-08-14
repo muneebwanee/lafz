8284d0d992a84579c8804fbd29d4dea83a61a7a3
Muneeb <muneebwanee@gmail.com>
2025-08-15 01:45:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,21 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Export static HTML for GitHub Pages
+  output: 'export',
+  basePath: '/lafz',          // repo name
+  assetPrefix: '/lafz/',      // ensures assets load correctly
+  trailingSlash: true,        // /about -> /about/index.html
+
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
+    unoptimized: true, // required for GitHub Pages (no image optimizer)
     remotePatterns: [
       {
         protocol: 'https',
