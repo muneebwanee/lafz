b477aa7b7a801b27f9e5464e6dc04aa0287fbfc3
Muneeb <i@muneeb.eu.org>
2025-10-20 22:34:04 +0330

    Add files via upload

---
+++ src/data/words/index.ts
@@ -0,0 +1,75 @@
+
+import type { Word } from '@/types';
+
+import { highFrequencyChapter1 } from './high-frequency/chapter-1';
+import { highFrequencyChapter2 } from './high-frequency/chapter-2';
+import { highFrequencyChapter3 } from './high-frequency/chapter-3';
+import { highFrequencyChapter4 } from './high-frequency/chapter-4';
+import { uniqueRootsChapter5 } from './unique-root/chapter-5';
+import { uniqueRootsChapter6 } from './unique-root/chapter-6';
+import { uniqueRootsChapter7 } from './unique-root/chapter-7';
+import { uniqueRootsChapter8 } from './unique-root/chapter-8';
+import { uniqueRootsChapter9 } from './unique-root/chapter-9';
+import { uniqueRootsChapter10 } from './unique-root/chapter-10';
+import { uniqueRootsChapter11 } from './unique-root/chapter-11';
+import { uniqueRootsChapter12 } from './unique-root/chapter-12';
+import { uniqueRootsChapter13 } from './unique-root/chapter-13';
+import { uniqueRootsChapter14 } from './unique-root/chapter-14';
+import { uniqueRootsChapter15 } from './unique-root/chapter-15';
+import { uniqueRootsChapter16 } from './unique-root/chapter-16';
+import { uniqueRootsChapter17 } from './unique-root/chapter-17';
+import { uniqueRootsChapter18 } from './unique-root/chapter-18';
+import { uniqueRootsChapter19 } from './unique-root/chapter-19';
+import { uniqueRootsChapter20 } from './unique-root/chapter-20';
+import { uniqueRootsChapter21 } from './unique-root/chapter-21';
+import { uniqueRootsChapter22 } from './unique-root/chapter-22';
+import { uniqueRootsChapter23 } from './unique-root/chapter-23';
+import { uniqueRootsChapter24 } from './unique-root/chapter-24';
+import { uniqueRootsChapter25 } from './unique-root/chapter-25';
+import { uniqueRootsChapter26 } from './unique-root/chapter-26';
+import { uniqueRootsChapter27 } from './unique-root/chapter-27';
+import { uniqueRootsChapter28 } from './unique-root/chapter-28';
+import { uniqueWordFormsChapter43 } from './unique-word-form/chapter-43';
+import { uniqueWordFormsChapter44 } from './unique-word-form/chapter-44';
+import { uniqueWordFormsChapter45 } from './unique-word-form/chapter-45';
+import { uniqueWordFormsChapter46 } from './unique-word-form/chapter-46';
+import { uniqueWordFormsChapter47 } from './unique-word-form/chapter-47';
+import { uniqueWordFormsChapter48 } from './unique-word-form/chapter-48';
+
+
+export const words: Word[] = [
+  ...highFrequencyChapter1,
+  ...highFrequencyChapter2,
+  ...highFrequencyChapter3,
+  ...highFrequencyChapter4,
+  ...uniqueRootsChapter5,
+  ...uniqueRootsChapter6,
+  ...uniqueRootsChapter7,
+  ...uniqueRootsChapter8,
+  ...uniqueRootsChapter9,
+  ...uniqueRootsChapter10,
+  ...uniqueRootsChapter11,
+  ...uniqueRootsChapter12,
+  ...uniqueRootsChapter13,
+  ...uniqueRootsChapter14,
+  ...uniqueRootsChapter15,
+  ...uniqueRootsChapter16,
+  ...uniqueRootsChapter17,
+  ...uniqueRootsChapter18,
+  ...uniqueRootsChapter19,
+  ...uniqueRootsChapter20,
+  ...uniqueRootsChapter21,
+  ...uniqueRootsChapter22,
+  ...uniqueRootsChapter23,
+  ...uniqueRootsChapter24,
+  ...uniqueRootsChapter25,
+  ...uniqueRootsChapter26,
+  ...uniqueRootsChapter27,
+  ...uniqueRootsChapter28,
+  ...uniqueWordFormsChapter43,
+  ...uniqueWordFormsChapter44,
+  ...uniqueWordFormsChapter45,
+  ...uniqueWordFormsChapter46,
+  ...uniqueWordFormsChapter47,
+  ...uniqueWordFormsChapter48,
+];
